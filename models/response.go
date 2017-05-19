238c035055b50e26f2260483d74ba9a987d16dd4
Bien Mandac <justinmandac@gmail.com>
2017-05-19 18:25:06 +0800

    Added json annotations

--- models/response.go
+++ models/response.go
@@ -6,10 +6,3 @@ type Response struct {
 	Message string      `json:"msg"`
 	Data    interface{} `json:"data"`
 }
-
-// ItemCategory - an item category
-type ItemCategory struct {
-	Name     string `json:"name"`
-	ID       int    `json:"id"`
-	ParentID int    `json:"parentId"`
-}
