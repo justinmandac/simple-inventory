917dbd2e1f381a4878ae05b900b0ff408642ba89
justinmandac <justinmandaca@gmail.com>
2017-05-20 11:18:02 +0800

    Prefix API call URIs with /api

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", handlers.MainHandler)
-	r.HandleFunc("/categories", handlers.GetCategoriesHandler).Methods("GET")
+	r.HandleFunc("/api/categories", handlers.GetCategoriesHandler).Methods("GET")
 
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
