dcc30f826e8870438c42ed5dc3231c1c4bcea4b9
justinmandac <justinmandac@gmail.com>
2017-05-26 07:05:02 +0800

    Updated db structure

--- inventory_v1.sql
+++ inventory_v1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2017 at 03:16 PM
+-- Generation Time: May 26, 2017 at 01:04 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -26,7 +26,6 @@ SET time_zone = "+00:00";
 -- Table structure for table `categories`
 --
 
-DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(256) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
@@ -47,14 +46,59 @@ INSERT INTO `categories` (`id`, `name`, `parentID`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `categories_map`
+--
+
+CREATE TABLE `categories_map` (
+  `id` int(11) NOT NULL,
+  `item_id` int(10) UNSIGNED NOT NULL,
+  `category_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories_map`
+--
+
+INSERT INTO `categories_map` (`id`, `item_id`, `category_id`) VALUES
+(1, 12, 3),
+(2, 4, 1),
+(3, 4, 2),
+(6, 4, 10),
+(7, 4, 10),
+(8, 4, 10);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `items`
 --
 
-DROP TABLE IF EXISTS `items`;
 CREATE TABLE `items` (
   `id` int(11) NOT NULL,
   `name` varchar(256) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   `description` varchar(512) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `name`, `description`) VALUES
+(4, 'boysen red', 'blue paint'),
+(5, 'boysen blue', 'blue paint'),
+(11, 'boysen green', 'blue paint'),
+(12, 'boysen yellow', 'blue paint');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stocks`
+--
+
+CREATE TABLE `stocks` (
+  `id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `quantity` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -69,11 +113,23 @@ ALTER TABLE `categories`
   ADD UNIQUE KEY `name` (`name`);
 
 --
+-- Indexes for table `categories_map`
+--
+ALTER TABLE `categories_map`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `items`
 --
 ALTER TABLE `items`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `stocks`
+--
+ALTER TABLE `stocks`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -85,9 +141,19 @@ ALTER TABLE `items`
 ALTER TABLE `categories`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
+-- AUTO_INCREMENT for table `categories_map`
+--
+ALTER TABLE `categories_map`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `stocks`
+--
+ALTER TABLE `stocks`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
