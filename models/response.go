4313407f3a5fb887cd7dddfcf330e951f3d11213
justinmandac <justinmandac@gmail.com>
2017-05-25 20:46:32 +0800

    Set Messsage field type to []string

--- models/response.go
+++ models/response.go
@@ -3,6 +3,6 @@ package models
 // Response - generic JSON response
 type Response struct {
 	Err     int         `json:"err"`
-	Message string      `json:"msg"`
+	Message []string    `json:"msg"`
 	Data    interface{} `json:"data"`
 }
